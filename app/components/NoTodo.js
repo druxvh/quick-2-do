8143fdea8f2cc586d866fce7a44851f88b1f9853
dm <druavhm@gmail.com>
2024-11-12 16:28:10 +0530

    fixed unescaped single quote

--- app/components/NoTodo.js
+++ app/components/NoTodo.js
@@ -5,10 +5,11 @@ import { TbFileSad } from "react-icons/tb";
 export default function NoTodo() {
   return (
     <div className=" h-60 w-full flex flex-col gap-2 justify-center items-center rounded-lg">
-
       <TbFileSad className="text-gray-400 size-[72px] stroke-[1.25]" />
 
-      <span className="text-gray-200 text-[10px] font-mono">No To-do's</span>
+      <span className="text-gray-200 text-[10px] font-mono">
+        No To-do&apos;s
+      </span>
     </div>
   );
 }
